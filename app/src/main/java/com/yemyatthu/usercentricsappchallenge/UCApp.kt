4a9ba8b9561a4e11b78fe86d90581f69d065b4ee
yemyatthu <thu.yemyat@shield.com>
2024-05-07 05:07:39 +0800

    remove initializer class and initialize in application class instead

--- app/src/main/java/com/yemyatthu/usercentricsappchallenge/UCApp.kt
+++ app/src/main/java/com/yemyatthu/usercentricsappchallenge/UCApp.kt
@@ -1,7 +1,20 @@
 package com.yemyatthu.usercentricsappchallenge
 
 import android.app.Application
+import com.usercentrics.sdk.Usercentrics
+import com.usercentrics.sdk.UsercentricsOptions
+import com.usercentrics.sdk.models.common.UsercentricsLoggerLevel
 import dagger.hilt.android.HiltAndroidApp
+import kotlin.math.log
 
 @HiltAndroidApp
-class UCApp: Application()
+class UCApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val options = UsercentricsOptions(
+            settingsId = BuildConfig.USERCENTRICS_SETTINGS_ID,
+            loggerLevel = if (BuildConfig.DEBUG) UsercentricsLoggerLevel.DEBUG else UsercentricsLoggerLevel.NONE
+        )
+        Usercentrics.initialize(this, options)
+    }
+}

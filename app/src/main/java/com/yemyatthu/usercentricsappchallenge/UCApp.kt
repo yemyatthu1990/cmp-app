ed28eded8c15023247296a6b34cf24dec770c530
yemyatthu <thu.yemyat@shield.com>
2024-05-07 05:42:54 +0800

    handle usercentrics initialize exception

--- app/src/main/java/com/yemyatthu/usercentricsappchallenge/UCApp.kt
+++ app/src/main/java/com/yemyatthu/usercentricsappchallenge/UCApp.kt
@@ -1,6 +1,7 @@
 package com.yemyatthu.usercentricsappchallenge
 
 import android.app.Application
+import android.util.Log
 import com.usercentrics.sdk.Usercentrics
 import com.usercentrics.sdk.UsercentricsOptions
 import com.usercentrics.sdk.models.common.UsercentricsLoggerLevel
@@ -11,10 +12,14 @@ import kotlin.math.log
 class UCApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        val options = UsercentricsOptions(
-            settingsId = BuildConfig.USERCENTRICS_SETTINGS_ID,
-            loggerLevel = if (BuildConfig.DEBUG) UsercentricsLoggerLevel.DEBUG else UsercentricsLoggerLevel.NONE
-        )
-        Usercentrics.initialize(this, options)
+        try {
+            val options = UsercentricsOptions(
+                settingsId = BuildConfig.USERCENTRICS_SETTINGS_ID,
+                loggerLevel = if (BuildConfig.DEBUG) UsercentricsLoggerLevel.DEBUG else UsercentricsLoggerLevel.NONE
+            )
+            Usercentrics.initialize(this, options)
+        } catch (e: Exception) {
+            Log.e(UCApp::class.java.simpleName, e.message.toString())
+        }
     }
 }
